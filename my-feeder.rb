bf4f599a4905342bb54d917618b3982dd8777e9c
Jonathan Andrade <jonathan@andrade.ninja>
2016-09-29 21:23:27 -0500

    listo para jalar

--- my-feeder.rb
+++ my-feeder.rb
@@ -1,6 +1,6 @@
 require 'tweetstream'
 require 'emoji_data'
-
+require 'cld'
 
 @client = Twitter::Streaming::Client.new do |config|
   config.consumer_key       = 'KRvH572Ep4usUK8eGOIKT5nLy'
@@ -8,16 +8,47 @@ require 'emoji_data'
   config.access_token        = '2385298681-W2l3LLZERhgrrlgIbFPpsSJkPNcQO3njU5La4wN'
   config.access_token_secret = '9aqz7p0rTRRX6qvHdFwnlkVKElr0zQs9HUsrIpsIisd4P'
 end
-# TweetStream::Client.new.track('term1', 'term2') do |status|
-#   puts "#{status.text}"
-# end
 
 TERMS = ['el','la','de','que','y','a','en','un','ser','se','no','haber','por','con','su','para','como','estar','tener','le','lo','lo','todo','pero','mas','hacer','o','poder','decir','este','ir','otro','ese','la','si','me','ya','ver','porque','dar','cuando','el','muy','sin','vez','mucho','saber','que','sobre','mi','alguno','mismo','yo','tambien','hasta','año','dos','querer','entre','asi','primero','desde','grande','eso','ni','nos','llegar','pasar','tiempo','ella','si','dia','uno','bien','poco','deber','entonces','poner','cosa','tanto','hombre','parecer','nuestro','tan','donde','ahora','parte','despues','vida','quedar','siempre','creer','hablar','llevar','dejar','nada','cada','seguir','menos','nuevo']
-@tracked,@skipped,@tracked_last,@skipped_last = 0,0,0,0
+@tracked,@no_emojis,@has_emojis,@total = 0,0,0,0
 
-@client.filter(track: TERMS.join(","), locations: "-122.75,36.8,-121.75,37.8", languages: 'es') do |status|
-	# disregard retweets
+@tweets_with_emojis = []
+@tweets_without_emojis = []
+
+@client.filter(track: TERMS.join(",")) do |status|
     next if status.retweet?
-	hasEmoji = EmojiData.scan(status.text).length > 0
-  	puts status.text if hasEmoji
+    language =  CLD.detect_language(status.text)
+    isSpanish = false
+
+    language.each do |k,v|
+    	isSpanish =  v == 'SPANISH' 
+    	break if isSpanish
+    end
+
+    next if not isSpanish
+	
+	emojis = EmojiData.scan(status.text)
+	next if emojis.length > 0
+
+	@tweets_without_emojis <<  status.text.dup
+	@total += 1
+
+	if @total % 20000 == 0 and @total > 0
+		puts "Saving batch " + @total.to_s
+		File.open('data/emoji_' + Time.now.utc.iso8601, 'w:UTF-8') do |file|
+			@tweets_without_emojis.each do |tweet|
+				text = tweet.gsub(/\r\n?/, "")
+				text = text.gsub(/@([a-z0-9_]+)/i, "")
+				text = text.gsub(/#([a-z0-9_]+)/i, "")
+				text = text.gsub(/(?:f|ht)tps?:\/[^\s]+/, '')
+				file.write(text + "\n")
+			end
+		end
+		@tweets_without_emojis = []
+	end
+
+	if  @total === 200000
+		return false
+	end
+
 end
